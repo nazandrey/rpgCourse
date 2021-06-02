60fd79f7e4c2a2edce37ad73e56a3a5b4e282948
nazandrey <nazandrbox@yandex.ru>
2021-06-02 23:45:12 +0300

    add click-to-move for player

--- Assets/_Main/Scripts/Mover.cs
+++ Assets/_Main/Scripts/Mover.cs
@@ -7,11 +7,31 @@ namespace RpgCourse
     public class Mover : MonoBehaviour
     {
         [SerializeField] private Transform target;
+        
+        private NavMeshAgent _navMeshAgent;
+        private Camera _mainCamera;
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
 
         private void Start()
         {
             GetComponent<NavMeshAgent>().SetDestination(target.position);
-            ;
+        }
+        
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out var hit))
+                {
+                    _navMeshAgent.SetDestination(hit.point);
+                }
+            }
         }
     }
 }
