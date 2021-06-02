e9c55cdf9180f15a66e2119f9543eca66b50c6fa
nazandrey <nazandrbox@yandex.ru>
2021-06-03 00:14:15 +0300

    make camera follow player with fixed angle

---
+++ Assets/_Main/Scripts/Follower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace RpgCourse
+{
+    public class Follower : MonoBehaviour
+    {
+        [SerializeField] private Transform objectToFollow;
+
+        private Vector3 _offset;
+        
+        private void Start()
+        {
+            _offset = transform.position - objectToFollow.position;
+        }
+
+        private void Update()
+        {
+            transform.position = objectToFollow.position + _offset;
+        }
+    }
+}
