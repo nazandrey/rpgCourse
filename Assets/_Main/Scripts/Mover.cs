13b60ed73545cc2c783db3a430d7bc0aabb9e572
nazandrey <nazandrbox@yandex.ru>
2021-06-19 15:01:50 +0300

    separate player control layer from mover (my take)

--- Assets/_Main/Scripts/Mover.cs
+++ Assets/_Main/Scripts/Mover.cs
@@ -4,40 +4,28 @@ using UnityEngine.AI;
 namespace RpgCourse
 {
     [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(Animator))]
     public class Mover : MonoBehaviour
     {
-        [SerializeField] private Transform target;
-        
         private NavMeshAgent _navMeshAgent;
-        private Camera _mainCamera;
         private Animator _animator;
 
         private void Awake()
         {
-            _mainCamera = Camera.main;
             _navMeshAgent = GetComponent<NavMeshAgent>();
             _animator = GetComponent<Animator>();
         }
 
-        private void Start()
-        {
-            GetComponent<NavMeshAgent>().SetDestination(target.position);
-        }
-        
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
-            {
-                var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out var hit))
-                {
-                    _navMeshAgent.SetDestination(hit.point);
-                }
-            }
-
             var globalVelocity = _navMeshAgent.velocity;
             var localVelocity = transform.InverseTransformVector(globalVelocity);
             _animator.SetFloat("Speed", localVelocity.z);
+        }
+
+        public void SetDestination(Vector3 targetPosition)
+        {
+            _navMeshAgent.SetDestination(targetPosition);
         }
     }
 }
---
+++ Assets/_Main/Scripts/PlayerDestinationChanger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace RpgCourse
+{
+    [RequireComponent(typeof(Mover))]
+    public class PlayerDestinationChanger : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+ 
+        private Camera _mainCamera;
+        private Mover _mover;
+
+        private void Awake()
+        {
+            _mover = GetComponent<Mover>();
+            _mainCamera = Camera.main;
+        }
+
+        private void Start()
+        {
+            _mover.SetDestination(target.position);
+        }
+    
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+            {
+                var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out var hit))
+                {
+                    _mover.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
