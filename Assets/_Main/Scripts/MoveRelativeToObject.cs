55e3dce0869acc1e149d935f9a7312ed3b9e65e2
nazandrey <nazandrbox@yandex.ru>
2021-06-19 14:49:07 +0300

    reset gameobject positions

---
+++ Assets/_Main/Scripts/MoveRelativeToObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace RpgCourse
+{
+    [ExecuteInEditMode]
+    public class MoveRelativeToObject : MonoBehaviour
+    {
+        [SerializeField] private Transform relatedObject;
+
+        private Vector3? _offset;
+        
+        private void OnValidate()
+        {
+            if (relatedObject)
+                _offset = transform.position - relatedObject.position;
+        }
+
+        private void Update()
+        {
+            if (relatedObject && _offset.HasValue)
+                transform.position = relatedObject.position + _offset.Value;
+        }
+    }
+}
